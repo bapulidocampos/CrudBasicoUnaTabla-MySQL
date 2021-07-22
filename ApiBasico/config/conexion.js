5533b32efa2e47cd4d314a4197300b6bcd8cbfe7
bapulidocampos <43184497+bapulidocampos@users.noreply.github.com>
2021-07-22 00:22:13 -0500

    Update conexion.js
    second commit

--- ApiBasico/config/conexion.js
+++ ApiBasico/config/conexion.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '4a4itri6',
+    password: '123456',
     port:'3306',
     database: 'db_basico'
 });
